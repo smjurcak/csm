780fb776bf1d95f2d5f7707974ab1b6be961be48
Alex Tang <alextang@cisco.com>
2016-10-24 18:07:20 -0700

    Support UTC for Download Dashboard

--- csmserver/csmserver.py
+++ csmserver/csmserver.py
@@ -151,6 +151,7 @@ from utils import create_directory
 from utils import create_temp_user_directory
 from utils import make_file_writable
 from utils import get_software_platform
+from utils import datetime_from_local_to_utc
 
 from server_helper import get_server_impl
 from wtforms.validators import Required
@@ -1414,7 +1415,7 @@ def get_files_from_csm_repository():
             row = {}
             row['image_name'] = filename
             row['image_size'] = str(statinfo.st_size)
-            row['downloaded_time'] = datetime.datetime.fromtimestamp(statinfo.st_mtime).strftime("%m/%d/%Y %I:%M %p")
+            row['downloaded_time'] = datetime_from_local_to_utc(datetime.datetime.fromtimestamp(statinfo.st_mtime)) 
             rows.append(row)
     
     return jsonify(**{'data': rows})
